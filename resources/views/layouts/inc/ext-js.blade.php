91bf26c5c79aafd01323b8f7af2e84bf6c1a334a
Nabila Arista <nabilaaristaputri16@gmail.com>
2022-03-11 20:08:47 +0700

    first commit

---
+++ resources/views/layouts/inc/ext-js.blade.php
@@ -0,0 +1,7 @@
+<!-- jQuery -->
+<script src="{{ url('/assets//plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ url('/assets//plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{ url('/assets//dist/js/adminlte.min.js')}}"></script>
+
